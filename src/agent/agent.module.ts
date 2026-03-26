9f604a31b0ad3b51ffbae2cf8b68ebd53bf4ef39
Allo <info@allo.dev>
2026-03-26 19:07:45 +0100

    Fix GiftRegistryService: import HttpModule in AgentModule

--- src/agent/agent.module.ts
+++ src/agent/agent.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AgentService } from './agent.service.js';
 import { GiftRegistryService } from './gift-registry.service.js';
 
 @Module({
+  imports: [HttpModule],
   providers: [AgentService, GiftRegistryService],
   exports: [AgentService],
 })
